612f4350103b27f423551f1e655254af4135de9c
maze <infimo000@gmail.com>
2024-11-25 21:00:48 +1000

    mod project

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -48919,7 +48919,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42351" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33383" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
