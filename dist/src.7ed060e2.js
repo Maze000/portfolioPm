590cf1db63273ee888ffdc3ccfd5b0dab92707a3
maze <infimo000@gmail.com>
2024-11-25 21:44:09 +1000

    mod project

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -48484,29 +48484,29 @@ var App = function App() {
     id: 1,
     img: _p2.default,
     descripcion: 'Proyecto 1',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 2,
     img: _p4.default,
     descripcion: 'Proyecto 2',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 3,
     img: _p6.default,
     descripcion: 'Proyecto 3',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 4,
     img: _p8.default,
     descripcion: 'Proyecto 4',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   //{ id: 5, img: image15p, descripcion: 'Proyecto 5' },
   {
     id: 6,
     img: _p12.default,
     descripcion: 'Proyecto 6',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   // { id: 7, img: image17pro, descripcion: 'Proyecto 7' },
   //{ id: 8, img: image18pro, descripcion: 'Proyecto 8' },
@@ -48514,36 +48514,36 @@ var App = function App() {
     id: 9,
     img: _p14.default,
     descripcion: 'Proyecto 9',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 10,
     img: _p15.default,
     descripcion: 'Proyecto 10',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   //{ id: 11, img: image21pro, descripcion: 'Proyecto 11' },
   {
     id: 12,
     img: _p17.default,
     descripcion: 'Proyecto 12',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   // { id: 13, img: image23pro, descripcion: 'Proyecto 13' },
   {
     id: 14,
     img: _p19.default,
     descripcion: 'Proyecto 14',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 15,
     img: _p20.default,
     descripcion: 'Proyecto 15',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 16,
     img: _p21.default,
     descripcion: 'Proyecto 16',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   //{ id: 17, img: image27pro, descripcion: 'Proyecto 17' },
   //{ id: 18, img: image28pro, descripcion: 'Proyecto 18' },
@@ -48552,36 +48552,36 @@ var App = function App() {
     id: 20,
     img: _p25.default,
     descripcion: 'Proyecto 20',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   //{ id: 21, img: image31pro, descripcion: 'Proyecto 21' },
   {
     id: 21,
     img: _p27.default,
     descripcion: 'Proyecto 22',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 21,
     img: _p28.default,
     descripcion: 'Proyecto 23',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   },
   //{ id: 21, img: image34pro, descripcion: 'Proyecto 24' },
   {
     id: 21,
     img: _p30.default,
     descripcion: 'Proyecto 25',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 21,
     img: _p31.default,
     descripcion: 'Proyecto 26',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }, {
     id: 21,
     img: _p32.default,
     descripcion: 'Proyecto 27',
-    link: 'https://storegitm1-production.up.railway.app/form-1'
+    link: 'https://ecommercemern-production.up.railway.app/form-1'
   }];
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "App"
@@ -48766,7 +48766,7 @@ var App = function App() {
       className: "proyect-box",
       key: proyecto.id
     }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("a", {
-      href: "https://storegitm1-production.up.railway.app/form-1",
+      href: "https://ecommercemern-production.up.railway.app/form-1",
       target: "_blank",
       className: "project"
     }, /*#__PURE__*/_react.default.createElement("img", {
